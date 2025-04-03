f46b4c35a71116383b80ab0d1de32466130359e6
psyofrelief <6faried@gmail.com>
2025-04-03 21:53:06 +0800

    fix: Update redirect for successful email verify

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -12,11 +12,12 @@ class VerifyEmailController extends Controller
     /**
      * Mark the authenticated user's email address as verified.
      */
-    public function __invoke(EmailVerificationRequest $request): RedirectResponse
-    {
+    public function __invoke(
+        EmailVerificationRequest $request
+    ): RedirectResponse {
         if ($request->user()->hasVerifiedEmail()) {
             return redirect()->intended(
-                config('app.frontend_url').'/dashboard?verified=1'
+                config("app.frontend_url") . "/?verified=1"
             );
         }
 
@@ -25,7 +26,7 @@ class VerifyEmailController extends Controller
         }
 
         return redirect()->intended(
-            config('app.frontend_url').'/dashboard?verified=1'
+            config("app.frontend_url") . "/?verified=1"
         );
     }
 }
